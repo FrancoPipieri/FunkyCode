2d281a29e8b2ad8de2a5ab8f49398d634a4f6af2
Franco Pipieri <pipierif98@gmail.com>
2022-06-29 21:09:34 -0300

    NavBar

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,25 @@
+import imagen from '../img/funkoyo.png';
+import './style.css';
+
+function NavBar(){
+    return(
+        <header>
+            <div className='flex nav'>
+                <img src={imagen} alt="" className='w-16 m-2' />
+                <div className='m-1.5 w-full enlaces'>
+                    <h1 className="text-3xl font-bold underline text-center">Funkys Coder</h1>
+                    <nav>
+                        <ul>
+                            <li className=" flex justify-end">
+                             <a href="/" className='m-2'>Home</a>
+                             <a href="/" className='m-2'>Catalogo</a>
+                             <a href="/" className='m-2'>Login</a>
+                            </li>
+                        </ul>
+                    </nav>
+                </div>
+            </div>
+        </header>
+    )
+}
+export default NavBar
