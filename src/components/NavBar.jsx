5032edaf5a9d87329b9b60b64a5c2c82ee41de14
Franco Pipieri <pipierif98@gmail.com>
2022-07-07 19:37:26 -0300

    navbar Responsive

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -152,30 +152,6 @@ function NavBar() {
     </Disclosure>
   )
 }
-/*
-function s(){
-    return(
-        <header>
-            <div className='flex nav'>
-                <img src={imagen} alt="" className='w-16 m-2' />
-                <div className='m-1.5 w-full enlaces'>
-                    <div className='flex'>
-                        <h1 className="text-3xl font-bold underline text-center titlePage">Funkys Coder</h1>
-                        <Icon/>
-                    </div>
-                    <nav>
-                        <ul>
-                            <li className=" flex justify-end">
-                             <a href="/" className='m-2'>Home</a>
-                             <a href="/" className='m-2'>Catalogo</a>
-                             <a href="/" className='m-2'>Login</a>
-                            </li>
-                        </ul>
-                    </nav>
-                </div>
-            </div>
-        </header>
-    )
-}*/
+
 
 export default NavBar;
