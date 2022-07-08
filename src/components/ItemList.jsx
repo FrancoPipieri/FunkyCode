aa8390f09db9a3eb564a793056e92689226771de
Franco Pipieri <pipierif98@gmail.com>
2022-07-08 20:55:46 -0300

    terminando desafio

--- src/components/ItemList.jsx
+++ src/components/ItemList.jsx
@@ -1,5 +1,19 @@
+import Item from './Item.jsx';
+import './main.css';
+
 function ItemList(props){
     return(
-        <div></div>
+    <div className="body">
+    <div className="max-w-2xl mx-auto py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8">
+      <h2 className="sr-only">Products</h2>
+
+      <div className="grid grid-cols-1 gap-y-10 sm:grid-cols-2 gap-x-6 lg:grid-cols-4 xl:grid-cols-4 xl:gap-x-8">
+        {props.funkos.map((datos) => (
+            <Item img={datos.filename} title={datos.title} price={datos.price} />
+        ))}
+      </div>
+    </div>
+  </div>
     )
 }
+export default ItemList;
--- src/components/ItemListConteiner.jsx
+++ src/components/ItemListConteiner.jsx
@@ -1,6 +1,5 @@
-import {useState , useEffect} from 'react';
-import './main.css';  
-import ItemCount from './ItemCount.jsx';
+import {useState , useEffect} from 'react';  
+import ItemList from './ItemList';
 
 function ItemListConteiner(props) {
   useEffect(()=>{
@@ -14,31 +13,8 @@ function ItemListConteiner(props) {
   const [funkos , setFunkos] = useState([]);
 
   return (
-    <div className="body">
-      <div className="max-w-2xl mx-auto py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8">
-        <h2 className="sr-only">Products</h2>
-
-        <div className="grid grid-cols-1 gap-y-10 sm:grid-cols-2 gap-x-6 lg:grid-cols-4 xl:grid-cols-4 xl:gap-x-8">
-          {funkos.map((datos) => (
-            <div className="group bg-white rounded-md">
-              <div className="w-full aspect-w-1 aspect-h-1 bg-gray-200 rounded-lg overflow-hidden xl:aspect-w-7 xl:aspect-h-8">
-                <img
-                  src={datos.filename}
-                  alt={datos.title}
-                  className="w-full h-full object-center object-cover group-hover:opacity-75"
-                />
-              </div>
-              <div className='text-center'>
-              <h3 className="mt-4 text-sm text-gray-700">{datos.title}</h3>
-              <h4 className="mt-4 text-sm text-gray-700">{datos.series}</h4>
-              <p className="mt-1 text-lg font-medium text-gray-900">${datos.price}</p>
-              </div>
-              <ItemCount stock={7} initial={1}/>
-            </div>
-          ))}
-        </div>
-      </div>
-    </div>)
+    <ItemList funkos={funkos}/>
+  )
 }
 
 export default ItemListConteiner;
