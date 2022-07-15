998be18e36594de9efac2fd5a6e76074ca7e05a5
Franco Pipieri <pipierif98@gmail.com>
2022-07-14 23:10:00 -0300

    desafio terminado

--- src/components/Conteiner/ItemDetailConteiner.jsx
+++ src/components/Conteiner/ItemDetailConteiner.jsx
@@ -2,22 +2,36 @@ import {useState , useEffect} from 'react';
 import ItemDetail from '../Detail/ItemDetail.jsx';
 
 function ItemDetailConteiner(props) {
-    useEffect(()=>{
-        setTimeout(() => {
+  //   useEffect(()=>{
+  //       setTimeout(() => {
+  //         fetch('/data.json')
+  //           .then(response => response.json())
+  //           .then(json => setDetail(json));
+  //       }, 2000);
+  //     },[])
+
+  //   const [detail, setDetail] = useState([]);
+      
+  //   if(detail.length == 0) return <h1>Loading...</h1>
+
+  // return (
+  //   <div>
+  //       <ItemDetail detail={detail[11]}/>
+  //   </div>
+  // )
+  useEffect(()=>{
+    setTimeout(() => {
           fetch('/data.json')
             .then(response => response.json())
-            .then(json => setDetail(json));
-        }, 2000);
-      },[])
+            .then(json => setItem(json.find(data => data.id == 12)));},2000);},[])
 
-    const [detail, setDetail] = useState([]);
-      
-    if(detail.length == 0) return <h1>Loading...</h1>
+const [getItem, setItem] = useState([])
+console.log(getItem)
+return(
+  <div>
+    <ItemDetail detail={getItem}/>
+  </div>
+)
 
-  return (
-    <div>
-        <ItemDetail detail={detail[11]}/>
-    </div>
-  )
 }
 export default ItemDetailConteiner;
