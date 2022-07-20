f46c9d9b1fffc8c464c121eec014740bfea11982
Franco Pipieri <pipierif98@gmail.com>
2022-07-20 19:28:03 -0300

    cart Widget

--- src/components/Navbar/CartWidget.jsx
+++ src/components/Navbar/CartWidget.jsx
@@ -2,7 +2,12 @@ import icon from '../../img/Cart.png'
 
 function IconWidget(){
     return(
+        <>
+        <div className='flex'>
         <img src={icon} alt="CartIcon" className='iconCart' />
+        <p className='mx-1'>0</p>
+        </div>
+        </>
     )
 }
 export default IconWidget
